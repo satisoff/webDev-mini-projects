23b18a7a5429ddbe911cfe9040fe0dd889f6191a
Satyam Vats <satyamvats2004@gmail.com>
2025-01-29 11:34:49 +0530

    Fixed minor bugs, Added more user friendly layout and animation to the input field.

--- loginWin/script.js
+++ loginWin/script.js
@@ -1,5 +1,7 @@
 const userInput = document.querySelector(".username-input");
 const passInput = document.querySelector(".pass-input");
+const bothInput = document.querySelectorAll(".form-box input");
+const bothInputTitle = document.querySelectorAll(".form-box p");
 const submit = document.getElementById("submit");
 const others = document.querySelectorAll(".other-box button");
 const container = document.querySelector(".container");
@@ -46,7 +48,7 @@ others.forEach((element) => {
     })
 
     element.addEventListener('mousedown', () => {
-        element.style.transform = "scale(0.95)";
+        element.style.transform = "scale(0.98)";
     })
     element.addEventListener('touchstart', () => {
         element.style.transform = "scale(0.9)";
@@ -56,5 +58,38 @@ others.forEach((element) => {
     })
     element.addEventListener("touchend", () => {
         element.style.transform = "scale(1)";
+    })
+})
+
+bothInput.forEach(element => {
+    element.addEventListener("focus", () => {
+        if (element === userInput) {
+                bothInputTitle[0].classList.add("input-title-final");
+                bothInputTitle[0].classList.remove("input-title-initial");
+                element.focus();
+        } else {
+            bothInputTitle[1].classList.add("input-title-final");
+            bothInputTitle[1].classList.remove("input-title-initial");
+            element.focus();
+        }
+    })
+    element.addEventListener("blur", () => {
+        if (element.value === '') {
+            if (element === userInput) {
+                bothInputTitle[0].classList.remove("input-title-final");
+                bothInputTitle[0].classList.add("input-title-initial");
+        } else {
+            bothInputTitle[1].classList.remove("input-title-final");
+            bothInputTitle[1].classList.add("input-title-initial");
+
+        }
+        }
+    })
+})
+bothInputTitle.forEach((title, indx) => {
+    title.addEventListener("click", () => {
+        title.classList.add("input-title-final");
+        title.classList.remove("input-title-initial");
+        (indx === 0) ? userInput.focus() : passInput.focus();
     })
 })
